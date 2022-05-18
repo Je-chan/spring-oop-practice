54b0ae3865dae086928137af9e4a6f5262390f25
je <jkyyc3@gmail.com>
2022-05-19 00:25:08 +0900

    #1 - CREATE : Init Spring OOP Practice

---
+++ src/test/java/com/java/oop/service/SortServiceTest.java
@@ -0,0 +1,24 @@
+package com.java.oop.service;
+
+import com.java.oop.logic.JavaSort;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortServiceTest {
+
+    private SortService sut = new SortService(new JavaSort<String>());
+
+    @Test
+    void test() {
+        // Given
+
+        // When
+        List<String> actual = sut.doSort(List.of("3", "5","2", "4", "1"));
+
+        // Then
+        assertEquals(List.of("1", "2", "3", "4","5"), actual);
+    }
+}
